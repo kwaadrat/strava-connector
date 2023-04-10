5affeed8df2d2d1e7a32838a97351b1f4302bd27
kwaadrat <kwaadrat@gmail.com>
2023-04-10 13:14:40 +0200

    .

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import urllib3
-import pandas as pd
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # API url to all activities.
@@ -15,6 +14,7 @@ access_token = "<token>"
 
 header = {'Authorization': 'Bearer ' + access_token}
 param = {'per_page': 1, 'page': 1}
+link = 
 my_dataset = requests.get(activites_url, headers=header, params=param).json()
 
 # Print data from API.
@@ -23,4 +23,4 @@ print(my_dataset)
 
 # Save data to csv
 
-my_dataset.to_csv('strava.csv')
+# my_dataset.to_csv('strava.csv')
