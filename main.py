23d219f6d66bcf8bf9dd5d709e7b46519edce5e6
kwaadrat <kwaadrat@gmail.com>
2023-05-08 20:56:37 +0200

    deleting separate files

--- main.py
+++ main.py
@@ -1,25 +1,93 @@
+import json
+import os
 import requests
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import time
 
-# API url to all activities.
+# Initial Settings
 
-activites_url = "https://www.strava.com/api/v3/athlete/activities"
+client_id = '93013'
+client_secret = '54eee8b78fcf488ed36e3f8c50a0165a375ed4f6'
+redirect_uri = 'http://localhost/'
 
-# Access token.
 
-access_token = "5b2f3a184eec098984a1ce31b426f452b8332334"
+def refreshToken():
 
-# Request.
+        # Authorization URL
+    request_url = f'http://www.strava.com/oauth/authorize?client_id={client_id}' \
+                    f'&response_type=code&redirect_uri={redirect_uri}' \
+                    f'&approval_prompt=force' \
+                    f'&scope=profile:read_all,activity:read_all'
 
-header = {'Authorization': 'Bearer ' + access_token}
-param = {'per_page': 1, 'page': 1}
-my_dataset = requests.get(activites_url, headers=header, params=param).json()
+        # User prompt showing the Authorization URL
+        # and asks for the code
+    print('Click here:', request_url)
+    print('Please authorize the app and copy&paste below the generated code!')
+    print('P.S: you can find the code in the URL')
+    code = input('Insert the code from the url: ')
 
-# Print data from API.
+    # Get the access token
+    token = requests.post(url='https://www.strava.com/api/v3/oauth/token',
+                       data={'client_id': client_id,
+                             'client_secret': client_secret,
+                             'code': code,
+                             'grant_type': 'authorization_code'})
 
-print(my_dataset)
+#Save json response as a variable
+    strava_token = token.json()
 
-# Save data to csv
+    with open('strava_token.json', 'w') as outfile:
+        json.dump(strava_token, outfile)
 
-# my_dataset.to_csv('strava.csv')
+refreshToken()
+
+# GET ATHLETE INFO
+
+# Read the token from the saved file
+with open('strava_token.json', 'r') as token:
+  data = json.load(token)
+
+# Get the access token
+access_token = data['access_token']
+
+# Build the API url to get athlete info
+athlete_url = f"https://www.strava.com/api/v3/athlete?50113221" \
+              f"access_token={access_token}"
+
+# Get the response in json format
+response = requests.get(athlete_url)
+athlete = response.json()
+
+# Print out the retrieved information
+print('RESTful API:', athlete_url)
+print('='* 5, 'ATHLETE INFO', '=' * 5)
+print('Name:', athlete['firstname'], '"' + athlete['username'] + '"', athlete['lastname'])
+print('Gender:', athlete['sex'])
+print('City:', athlete['city'], athlete['country'])
+print('Strava athlete from:', athlete['created_at'])
+
+# GET SINGLE ACTIVITY
+
+# Read the token from the saved file
+with open('strava_token.json', 'r') as token:
+  data = json.load(token)
+
+# Build the API url to get activities data
+activities_url = f"https://www.strava.com/api/v3/athlete/activities?" \
+          f"access_token={access_token}"
+print('RESTful API:', activities_url)
+
+# Get the response in json format
+response = requests.get(activities_url)
+activity = response.json()[5]
+
+# Print out the retrieved information
+print('='*5, 'SINGLE ACTIVITY', '='*5)
+print('Athlete:', athlete['firstname'], athlete['lastname'])
+print('Name:', activity['name'])
+print('Date:', activity['start_date'])
+print('Disance:', activity['distance'], 'm')
+print('Average Speed:', activity['average_speed'], 'm/s')
+print('Max speed:', activity['max_speed'], 'm/s')
+print('Moving time:', round(activity['moving_time'] / 60, 2), 'minutes')
+print('Location:', activity['location_city'], 
+      activity['location_state'], activity['location_country'])
